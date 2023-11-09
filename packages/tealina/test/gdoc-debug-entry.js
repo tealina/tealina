da116afb56310804f829e7c1d78cc687f0d481e5
Neo Yeo <neo@tealina.dev>
2023-11-09 09:58:09 +0800

    release: new version

--- packages/tealina/test/gdoc-debug-entry.js
+++ packages/tealina/test/gdoc-debug-entry.js
@@ -3,7 +3,7 @@ import { writeFileSync } from 'fs'
 import { parseDeclarationFile } from '../src/utils/parseDeclarationFile.js'
 
 const doc = parseDeclarationFile({
-  entries: ['temp/fastify/restful/server/types/api-v1.d.ts'],
-  tsconfigPath: 'temp/express/post-get/server/tsconfig.json',
+  entries: ['temp/fastify/post-get/server/types/api-v1.d.ts'],
+  tsconfigPath: 'temp/fastify/restful/server/tsconfig.json',
 })
 writeFileSync('temp/doc.json', JSON.stringify(doc, null, 2))
