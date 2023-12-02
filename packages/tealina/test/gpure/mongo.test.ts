e99b807244e8e6881fd33cfe00738a5be6b10e9c
tealina <neo@tealina.dev>
2023-12-02 14:28:37 +0800

    test: use vitest snapshot

--- packages/tealina/test/gpure/mongo.test.ts
+++ packages/tealina/test/gpure/mongo.test.ts
@@ -1,51 +1,6 @@
 import { expect, test } from 'vitest'
 import { workflow } from '../../src/commands/gpure'
 
-const snapshot = [
-  'interface User{',
-  '  /** @default {auto()} */',
-  '  id: string',
-  '  email: string',
-  '  name: string | null',
-  '  address: Address | null',
-  '}',
-  '',
-  'interface UserCreateInput{',
-  '  email: string',
-  '  name?: string',
-  '  address?: AddressCreateInput',
-  '}',
-  '',
-  'interface UserUpdateInput{',
-  '  /** @default {auto()} */',
-  '  id?: string',
-  '  email?: string',
-  '  name?: string',
-  '  address?: AddressCreateInput',
-  '}',
-  '',
-  'interface Address{',
-  '  street: string',
-  '  city: string',
-  '  state: string',
-  '  zip: string',
-  '}',
-  '',
-  'interface AddressCreateInput{',
-  '  street: string',
-  '  city: string',
-  '  state: string',
-  '  zip: string',
-  '}',
-  '',
-  'interface AddressUpdateInput{',
-  '  street?: string',
-  '  city?: string',
-  '  state?: string',
-  '  zip?: string',
-  '}',
-  '',
-]
 test('mongo database has composite type', async () => {
   const result = await workflow('test/utils/mock/mongo.prisma', {
     typeRemap: t => (t == 'DateTime' ? 'number' : null),
@@ -60,5 +15,5 @@ test('mongo database has composite type', async () => {
       ],
     },
   })
-  expect(result).deep.eq(snapshot)
+  expect(result).matchSnapshot()
 })
