af893048ccacd775fb0aa29a30b16f16a1961959
tealina <neo@tealina.dev>
2023-12-02 15:12:58 +0800

    fix:  not follow strict rule

--- packages/create-tealina/template/server/express/post-get/src/middlewares/setupApiHeaders.ts
+++ packages/create-tealina/template/server/express/post-get/src/middlewares/setupApiHeaders.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from 'express'
 
-const setupApiHeaders: RequestHandler = (req, res, next) => {
+const setupApiHeaders: RequestHandler = (_req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET')
   res.setHeader('Content-Type', 'application/json;charset=utf-8')
   next()
