f45d2eecaced43c164103c29a103f8d85628c0fc
Neo <neo@tealina.dev>
2025-02-04 01:44:50 +0800

    build: not export raw create fn

--- packages/tealina-client/src/index.ts
+++ packages/tealina-client/src/index.ts
@@ -1,8 +1,6 @@
 export {
   createAxiosClient,
-  createRawAxiosClient,
   createAxiosRPC,
-  createRawAxiosRPC,
 } from './axios/index'
 
 export { createFetchClient, createFetchRPC } from './fetch'
