ef5b9d634b5591efbbe3c72445335522ae8e4470
Neo <neo@tealina.dev>
2025-08-15 02:15:23 +0800

    test: fix dir not found

--- packages/tealina/test/gdoc/toOpenApi.test.ts
+++ packages/tealina/test/gdoc/toOpenApi.test.ts
@@ -5,7 +5,7 @@ import { convertToOpenApiJson } from '../../src/utils/genOpenApi'
 
 test(' api doc Conver to Open API', async () => {
   console.log(path.resolve())
-  const entry = path.resolve('test/mock/api-v1.json')
+  const entry = path.resolve('test/gdoc/mock/api-v1.json')
   const input = await readFile(entry)
   const output = convertToOpenApiJson(JSON.parse(input.toString()))
   await writeFile('./output.json', JSON.stringify(output, null, 2))
