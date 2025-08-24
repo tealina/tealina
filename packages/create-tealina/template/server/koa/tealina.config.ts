33dc2b7ac0007b5d875a2a03bb58dfc94fa13f7c
Neo <neo@tealina.dev>
2025-08-25 00:21:57 +0800

    feat: add default template

---
+++ packages/create-tealina/template/server/koa/tealina.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, TemplateContext } from 'tealina'
+
+export default defineConfig({
+  template: {
+    handlers: [
+      {
+        name: '',
+        alias: '*',
+        generateFn: generateBasicCode,
+      },
+    ],
+  },
+  typesDir: 'types',
+  gtype: {
+    output: '../shared-type/pure.d.ts',
+  },
+})
+
+function generateBasicCode({ relative2api }: TemplateContext) {
+  return [
+    `import type { AuthedHandler } from '${relative2api}/../types/handler.js'`,
+    `import { convention } from '${relative2api}/convention.js'`,
+    '',
+    `/** TODO: describe what it does */`,
+    `const handler: AuthedHandler = async (ctx) => {`,
+    '  throw new Error("Unimplement handler")',
+    '}',
+    '',
+    `export default convention(handler)`,
+    '',
+  ].join('\n')
+}
