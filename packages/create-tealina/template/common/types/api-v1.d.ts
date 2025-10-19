25daa9d43bcbfec96ad84c40fe33c96337f2b398
Neo <neo@tealina.dev>
2025-10-20 01:36:37 +0800

    fix: misplaced files

--- packages/create-tealina/template/common/types/api-v1.d.ts
+++ packages/create-tealina/template/common/types/api-v1.d.ts
@@ -1,12 +1,24 @@
-import type apis from '../src/api-v1/index.js'
-import type { ResolveApiType } from './handler.js'
+import apis from '../src/api-v1/index.js'
+import type {
+  ResolveApiTypeForClient,
+  ResolveApiTypeForDoc,
+} from './handler.js'
 
 type RawApis = typeof apis
+
 /**
  * The first type export in this file is used for API documentation generation.
  * This file is also specified in package.json's `export.types` field,
  * making it discoverable and importable by other packages.
  */
-export type ApiTypesRecord = {
-  [Method in keyof RawApis]: ResolveApiType<Awaited<RawApis[Method]>['default']>
+export type ApiTypesForDoc = {
+  [Method in keyof RawApis]: ResolveApiTypeForDoc<
+    Awaited<RawApis[Method]>['default']
+  >
+}
+
+export type ApiTypesForClient = {
+  [Method in keyof RawApis]: ResolveApiTypeForClient<
+    Awaited<RawApis[Method]>['default']
+  >
 }
