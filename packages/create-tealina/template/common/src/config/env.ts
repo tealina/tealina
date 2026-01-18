88920b8ac120ff511d36567408cb545c7ffe7f72
Neo <neo@tealina.dev>
2026-01-18 18:37:37 +0800

    fix: env load error

--- packages/create-tealina/template/common/src/config/env.ts
+++ packages/create-tealina/template/common/src/config/env.ts
@@ -1,4 +1,14 @@
 import { z } from 'zod'
+import { config } from 'dotenv'
+
+const NODE_ENV = process.env.NODE_ENV ?? 'development'
+const envFiles = [
+  '.env',
+  `.env.${NODE_ENV}`,
+  `.env.local`,
+  `.env.${NODE_ENV}.local`,
+]
+config({ path: envFiles })
 
 const schame = z.object({
   PORT: z
@@ -9,6 +19,7 @@ const schame = z.object({
       return num > 0 && num < 65536
     }, 'PORT should be in range 0-65536'),
   NODE_ENV: z.enum(['development', 'test', 'production']),
+  DATABASE_URL: z.string(),
 })
 
 schame.parse(process.env)
--- packages/create-tealina/template/common/src/db/prisma.ts
+++ packages/create-tealina/template/common/src/db/prisma.ts
@@ -1,8 +1,13 @@
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client.js'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL,
+})
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const db = globalForPrisma.prisma || new PrismaClient()
+const db = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 export { db }
